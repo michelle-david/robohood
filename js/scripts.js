21e42f6c589a834b52448e5bd0ae93ca8a1f5cba
Michelle David <hilarity.ensues@gmail.com>
2020-07-25 16:52:10 -0700

    add js logic

--- js/scripts.js
+++ js/scripts.js
@@ -6,9 +6,20 @@
 $(document).ready(function() {
   $("form#form").submit(function() {
     event.preventDefault();
+    // below converts user input into a string
     const userNumber = parseInt($("input#userInput").val());
 
-    console.log(userNumber)
+    console.log(userNumber) // don't forget to remove!
+    
+    const roboNumbers = ["1", "2", "3"];
+
+  // below converts user string into an array
+  let userArray = [];
+  for (let i=0; i <= userNumber ; i++){
+  userArray.push(i);
+  console.log(userArray)
+ }
+
 
   });
-  });
+})
