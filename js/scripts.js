722c900b399643f7672fa38765ea1a6de1d4f1f8
Michelle David <hilarity.ensues@gmail.com>
2020-07-26 17:11:36 -0700

    add logic for first three specs

--- js/scripts.js
+++ js/scripts.js
@@ -3,23 +3,40 @@
 
 // User Logic
 
-$(document).ready(function() {
-  $("form#form").submit(function() {
+$(document).ready(function () {
+  $("form#form").submit(function () {
     event.preventDefault();
     // below converts user input into a string
     const userNumber = parseInt($("input#userInput").val());
 
     console.log(userNumber) // don't forget to remove!
-    
-    const roboNumbers = ["1", "2", "3"];
 
-  // below converts user string into an array
-  let userArray = [];
-  for (let i=0; i <= userNumber ; i++){
-  userArray.push(i);
-  console.log(userArray)
- }
+    //below converts robo numbers into an array
+    const roboNumbers = [1, 2, 3];
 
+    //below is emtpy array for robotspeak
+    let roboArray = [];
+
+    // below converts user string into an array
+    let userArray = [];
+    console.log(userArray) // comment out
+    for (let i = 0; i <= userNumber; i++) {
+      userArray.push(i);
+      console.log(userArray) // comment out
+    }
+
+    const robo1 = 1
+    const robo2 = 2
+    const robo3 = 3
+    let newArray1 = userArray.indexOf(robo1);
+    let newArray2 = userArray.indexOf(robo2)
+    let newArray3 = userArray.indexOf(robo3)
+
+    userArray.splice(newArray1, 1, "Beep");
+    userArray.splice(newArray2, 1, "Boop");
+    userArray.splice(newArray3, 1, "Won't you be my neighbor?")
+
+    console.log(userArray)
 
   });
 })
--- readme.md
+++ readme.md
@@ -14,11 +14,11 @@
 
 * spec: the program returns a range of numbers from 0 to the user's inputted number (example input: "4", output: "0, 1, 2, 3, 4")
 
-* spec: if number contains the digit "1", all digits are replaced with the word string "Beep!"
+* spec: if number contains the digit "1", all digits are replaced with the word string "Beep!" (example input: "3", output: "0, Beep!, 2, 3")
 
-* spec: if number contains the digit "2", all digits are replaced with the word string "Boop!"
+* spec: if number contains the digit "2", all digits are replaced with the word string "Boop!" (example input: "2", output: "0, 1, Boop!, 3")
 
-* spec: if number contains the digit "3", all digits are replaced with the word string "Won't you be my neighbor?"
+* spec: if number contains the digit "3", all digits are replaced with the word string "Won't you be my neighbor?" (example input: "31", output: "0, 1, 2, Won't you be my neighbor? ... 28, 29, Won't you be my neighbor?, Won't you be my neighbor?")
 
 ## technologies used
 
